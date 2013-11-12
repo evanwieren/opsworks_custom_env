b8e4ea12bccd0c3a4f0d471baacba9eefb41b8f4
Eric VanWieren <evanwieren@gmail.com>
2013-11-12 15:54:26 -0800

    Trying a change for writing yml file directly.

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -1,6 +1,16 @@
-if node[:opsworks][:instance][:layers].include?('rails-app')
+node[:deploy].each do |app, deploy|
+  if node[:custom_env].has_key?(app) && node[:opsworks][:instance][:layers].include?('rails-app')
+    ruby_block "application_yml_writer" do
 
-  include_recipe "opsworks_custom_env::restart_command"
-  include_recipe "opsworks_custom_env::write_config"
+      inner_node = node
+      block do
+        yml_file_path = ::File.join(inner_node[:deploy][app][:deploy_to], 'config', 'application.yml')
+#        if ::File.exists?(yml_file_path)
+        ::File.open(yml_file_path, 'w') do |yml_file|
+          yml_file.write(YAML.dump(inner_node[:custom_env][app]))
+        end
 
+      end
+    end
+  end
 end
