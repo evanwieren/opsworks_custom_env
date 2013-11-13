e8c5f7f36877a30dfd499080fe25f2867579f8b1
Eric VanWieren <evanwieren@gmail.com>
2013-11-12 16:38:44 -0800

    changing the path to the file

--- opsworks_custom_env/recipes/configure.rb
+++ opsworks_custom_env/recipes/configure.rb
@@ -4,7 +4,7 @@ node[:deploy].each do |app, deploy|
 
       inner_node = node
       block do
-        yml_file_path = ::File.join(inner_node[:deploy][app][:deploy_to], 'config', 'application.yml')
+        yml_file_path = ::File.join(inner_node[:deploy][app][:deploy_to], 'current', 'config', 'application.yml')
 #        if ::File.exists?(yml_file_path)
         ::File.open(yml_file_path, 'w') do |yml_file|
           yml_file.write(YAML.dump(inner_node[:custom_env][app]))
